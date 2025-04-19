34694530794a8e3e1a20878f40cbf2d4d3ef96aa
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-19 12:38:39 -0300

    fix: include BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { WalletConnectionProvider } from './contexts/WalletContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WalletConnectionProvider>
-      <App />
-    </WalletConnectionProvider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <WalletConnectionProvider>
+        <App />
+      </WalletConnectionProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
