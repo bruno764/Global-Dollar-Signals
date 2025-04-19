10e11a6c084cfe2526cf6de14feb07f4df6f8cbb
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-19 13:51:10 -0300

    refactor: remove wallet connection and keep only email login

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { WalletConnectionProvider } from './contexts/WalletContext';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <WalletConnectionProvider>
-        <App />
-      </WalletConnectionProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
